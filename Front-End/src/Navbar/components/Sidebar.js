7ad5826e67f5a25be5a200ca5b416add1ba6fd89
Clarkbrennin <111391664+Clarkbrennin@users.noreply.github.com>
2023-01-14 21:52:32 -0700

    Added Side-Bar

---
+++ Front-End/src/Navbar/components/Sidebar.js
@@ -0,0 +1,14 @@
+import "./SidebarStyles.css"
+import React from "react";
+
+function Sidebar() {
+
+    return (
+        <div className="Navbar-left">
+            <p>h</p>
+        </div>
+        )
+    }
+    
+    
+    export default Sidebar;
--- Front-End/src/routes/How.js
+++ Front-End/src/routes/How.js
@@ -1,10 +1,12 @@
 import How from "../Howto/components/Howto";
 import Navbar from "../Navbar/components/Navbar"
+import Sidebar from "../Navbar/components/Sidebar"
 
 
 function Howdo () {
     return (
         <>
+        <Sidebar />
         <Navbar />
         <How />
         </>
