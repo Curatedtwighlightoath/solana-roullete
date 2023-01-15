f88a763f36b8c001b17d6845ebe98385d9656472
Clarkbrennin <111391664+Clarkbrennin@users.noreply.github.com>
2023-01-15 00:01:28 -0700

    Got server to run

--- Front-End/src/Navbar/components/Sidebar.js
+++ Front-End/src/Navbar/components/Sidebar.js
@@ -1,11 +1,36 @@
-import "./SidebarStyles.css"
+import "./SidebarStyles.css";
 import React from "react";
 
 function Sidebar() {
 
     return (
         <div className="Navbar-left">
-            <p>h</p>
+        <div className="but-left">
+
+        <a href="/" rel="noreferrer">
+            <button className="false">Home</button>
+        </a>
+
+        <a href="/howtoflip" rel="noreferrer">
+            <button className="false">How To Flip</button>
+        </a>
+
+        <a href="/leaderboard" rel="noreferrer">
+            <button className="false">LeaderBoard</button>
+        </a>
+
+        <a href="/register" rel="noreferrer">
+            <button className="false">Register / Login </button>
+        </a>
+
+        <a href="/userPost" rel="noreferrer">
+            <button className="false">Talk</button>
+        </a>
+
+        <a href="/Rock" rel="noreferrer">
+        <button className="false">Rock-Paper-Scissors</button>
+        </a>
+        </div>
         </div>
         )
     }
