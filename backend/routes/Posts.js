4147dfc3f89644eb95db5a99b057e413736a07e2
Clarkbrennin <111391664+Clarkbrennin@users.noreply.github.com>
2023-01-15 16:44:26 -0700

    Posts and Pull working

--- backend/routes/Posts.js
+++ backend/routes/Posts.js
@@ -1,23 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const { Posts } = require("../models");
 
 router.get("/", async (req, res) => {
-    const listofPosts = await Posts.findAll();
-    res.json(listofPosts);
-});
-
-router.get("/byId/:id", async (req, res) => {
-    const id = req.params.id;
-    const post = await Posts.findByPk(id);
-    res.json(post);
+  const listOfPosts = await Posts.findAll();
+  res.json(listOfPosts);
 });
 
 router.post("/", async (req, res) => {
-    const post = req.body;
-    await Posts.create(post);
-    res.json(post);
+  const post = req.body;
+  await Posts.create(post);
+  res.json(post);
 });
 
-
-module.exports = router 
+module.exports = router;
