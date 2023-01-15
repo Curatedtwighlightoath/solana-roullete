aa30c2cbf124dccdf01d6ac35f850dd95d07adc8
Clarkbrennin <111391664+Clarkbrennin@users.noreply.github.com>
2023-01-14 18:50:37 -0700

    Changes to be basic
    :)

---
+++ src/Howto/components/Howto.js
@@ -0,0 +1,22 @@
+import "./Howto.css";
+import { FaTwitter } from "react-icons/fa";
+import { FaDiscord } from "react-icons/fa";
+import Magiceden from "../assets/magiceden.png"
+
+function Signup () {
+    return (
+    <div className="how-top">
+        <div className="how-info">
+        <h1> How To Play</h1>
+        <p>1. Login in to an account. (Get Account)</p>
+        <p>2. Pick either heads or tails.</p>
+        <p>3. Select your desired flip amount.</p>
+        <p>4. Click approve and wait for coin to spin</p>
+        <p>5. Congrats, you’re now a degenerate.</p>
+        </div>
+    </div>
+    )
+
+}
+
+export default Signup;
---
+++ src/routes/How.js
@@ -0,0 +1,12 @@
+import How from "../Howto/components/Howto";
+
+
+function Howdo () {
+    return (
+        <>
+        <How />
+        </>
+        );
+    }
+
+export default Howdo;
