9ad443ed40f8b41847e69f198745d9acd694952d
Glaucus <stdxvintage@gmail.com>
2023-01-16 03:03:01 -0500

    update

---
+++ app/models/index.js
@@ -0,0 +1,22 @@
+const config = require("../config/config")
+const Sequelize = require("sequelize")
+const sequelize = new Sequelize(config.DB, config.DB_USER, config.DB_PASSWORD, {
+    host: config.DB_HOST,
+    dialect: config.dialect,
+    operatorsAliases: false,
+    pool: {
+        max: config.pool.max,
+        min: config.pool.min,
+        acquire: config.pool.acquire,
+        idle: config.pool.idle
+    },
+    logging: false
+})
+
+const db = {}
+db.Sequelize = Sequelize
+db.sequelize = sequelize
+
+db.entries = require("./entries.modal")(sequelize, Sequelize);
+
+module.exports = db;
