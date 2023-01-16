e59d2397ba0d795715baff619e91e489ebb3225d
Clarkbrennin <111391664+Clarkbrennin@users.noreply.github.com>
2023-01-15 17:31:59 -0700

    Backend send and pull on page done

--- Front-End/src/Howto/components/Howto.js
+++ Front-End/src/Howto/components/Howto.js
@@ -1,6 +1,19 @@
 import "./Howto.css";
+import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function Signup () {
+    const [listofPosts, setlistofPosts] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:3001/posts").then((response) => {
+            setlistofPosts(response.data);
+        });
+    }, []);
+
+
+
     return (
     <div className="how-top">
         <h1> How To Play</h1>
@@ -9,6 +22,16 @@ function Signup () {
         <p>3. Select your desired flip amount.</p>
         <p>4. Click approve and wait for coin to spin</p>
         <p>5. Congrats, you’re now a degenerate.</p>
+
+        {listofPosts.map((value, key) => {
+            return (
+                <div className="list-post" >
+                <div className="list-title"> {value.title} </div>
+                <div className="list-body"> {value.postText} </div>
+                <div className="list-footer"> {value.username} </div>
+                </div>
+            );
+        })}
 
     </div>
     
